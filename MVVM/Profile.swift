7c52a0998324c973b464d73d5efc4f9285ff3b31
Konstantin <grahovsky@gmail.com>
2018-09-23 23:33:20 +0300

    MVC

---
+++ MVVM/Profile.swift
@@ -0,0 +1,15 @@
+//
+//  Profile.swift
+//  MVVM
+//
+//  Created by Konstantin on 23.09.2018.
+//  Copyright © 2018 Konstantin. All rights reserved.
+//
+
+import Foundation
+
+struct Profile {
+    var name: String
+    var secondName: String
+    var age: Int
+}
--- MVVM/ViewController.swift
+++ MVVM/ViewController.swift
@@ -10,9 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var secondNameLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+    
+    var profile: Profile? {
+        didSet {
+            guard let profile = profile else { return }
+            
+            self.nameLabel.text = profile.name
+            self.secondNameLabel.text = profile.secondName
+            self.ageLabel.text = "\(profile.age)"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        profile = Profile(name: "John", secondName: "Smith", age: 33)
+        
+        
     }
 
 
