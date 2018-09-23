8ddcc0f875aec7058de0795a73fd88d6efc099f1
Konstantin <grahovsky@gmail.com>
2018-09-23 23:45:08 +0300

    #describing #didSet

---
+++ MVVM/ViewModel.swift
@@ -0,0 +1,27 @@
+//
+//  ViewModel.swift
+//  MVVM
+//
+//  Created by Konstantin on 23.09.2018.
+//  Copyright © 2018 Konstantin. All rights reserved.
+//
+
+import Foundation
+
+class ViewModel {
+    
+    private var profile = Profile(name: "John", secondName: "Smith", age: 33)
+    
+    var name: String {
+        return profile.name
+    }
+    
+    var secondName: String {
+        return profile.secondName
+    }
+    
+    var age: String {
+        return String(describing: profile.age)
+    }
+    
+}
