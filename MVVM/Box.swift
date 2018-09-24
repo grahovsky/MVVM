d97a814ad1d81335c871feda528c7893a093dfd9
Konstantin <grahovsky@gmail.com>
2018-09-24 23:20:03 +0300

    #Box #Listner #<T> #$0 #delay #@escaping #[unowned self] #очень прикольная штука

---
+++ MVVM/Box.swift
@@ -0,0 +1,32 @@
+//
+//  Box.swift
+//  MVVM
+//
+//  Created by Konstantin on 24/09/2018.
+//  Copyright © 2018 Konstantin. All rights reserved.
+//
+
+import Foundation
+
+class Box<T> {
+    
+    typealias Listener = (T) -> ()
+    
+    var listener: Listener?
+    
+    var value: T {
+        didSet {
+            listener?(value)
+        }
+    }
+    
+    func bind(listener: @escaping Listener) {
+        self.listener = listener
+        listener(value)
+    }
+    
+    init(_ value: T) {
+        self.value = value
+    }
+    
+}
